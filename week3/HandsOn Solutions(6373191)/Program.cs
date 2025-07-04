178b7ab4294ce9d11a6de853badc7bde0d42a718
Yaga Gopinadh <gopinadhyaga@gmail.com>
2025-07-04 08:49:33 +0530

    Add files via upload

---
+++ week3/HandsOn Solutions(6373191)/Program.cs
@@ -0,0 +1,34 @@
+﻿using entity_core.Data;
+using entity_core.Models;
+using Microsoft.EntityFrameworkCore;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var context = new AppDbContext();
+
+
+        var singleProduct = await context.Products
+            .Include(p => p.Category)
+            .SingleOrDefaultAsync(p => p.Id == 6);
+
+        if (singleProduct != null)
+        {
+            Console.WriteLine($"\nSingle Product (Id = 1):");
+            Console.WriteLine($"Product: {singleProduct.Name}, Price: ₹{singleProduct.Price}, Category: {singleProduct.Category.Name}");
+        }
+        else
+        {
+            Console.WriteLine("\nNo product found with Id = 1.");
+        }
+
+ 
+        var expensive = await context.Products
+            .Include(p => p.Category)
+            .FirstOrDefaultAsync(p => p.Price > 50000);
+
+        Console.WriteLine("\nExpensive Product:");
+        Console.WriteLine($"Product: {expensive?.Name}, Price: ₹{expensive?.Price}, Category: {expensive?.Category.Name}");
+    }
+}
