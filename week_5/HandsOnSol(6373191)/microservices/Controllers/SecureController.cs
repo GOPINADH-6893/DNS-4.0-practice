7bca99e8b2d34cc6242a4ce86afac875e903fd2e
Yaga Gopinadh <gopinadhyaga@gmail.com>
2025-07-21 01:45:58 +0530

    Add files via upload

---
+++ week_5/HandsOnSol(6373191)/microservices/Controllers/SecureController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SecureController : ControllerBase
+{
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        return Ok("This is a protected endpoint. You are authorized!");
+    }
+
+    [HttpGet("data")]
+    [Authorize]
+    public IActionResult GetSecureData()
+    {
+        return Ok("This is protected data.");
+    }
+}
